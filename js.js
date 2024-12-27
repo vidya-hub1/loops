ecc41d509e13e3a8aedde3f928045018de85fe46
shashank boddula <vidyaboddula625@gmail.com>
2024-12-27 16:39:57 +0530

    file added

---
+++ js.js
@@ -0,0 +1,66 @@
+for(var a=5; a<=15;a++){
+    console.log(a)
+}
+for(var a=20; a>=12; a--){
+    console.log(a)
+}
+
+for(var a=5; a<=15;a++){
+    }
+    console.log(a)
+
+var a=15;
+while(a<=25){
+       console.log(a)
+       a++
+}
+
+var a=19;
+while(a>11){
+    console.log(a)
+    a--
+}
+
+var a=6;
+do{
+    a++
+    console.log(a)
+}while(a<10)
+
+var a=13;
+do{
+    console.log(a)
+    a++
+}while(a>=19)
+
+var a=30;
+do{
+    a--
+    console.log(a)
+}while(a>25)
+
+
+ var a=26;
+ do{
+    console.log(a)
+    a--
+ }while(a>20)
+
+
+for(var i=2; i<=10; i+=2){
+    console.log(i)
+}
+
+for(var i=10; i>=1; i-=2){
+    console.log(i)
+}
+
+for(var i=1; i<=10; i+=2){
+    console.log(i)
+}
+
+for(var i=9; i>=1; i-=2){
+    console.log(i)
+}
+
+
